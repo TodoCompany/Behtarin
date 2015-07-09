f9573237a6245ae0d83614e2fb1edd3e5460de6a
LohushD <dmytro.lohush@gmail.com>
2015-07-09 14:26:37 +0300

    added tripAdvisorWebUrl to mainlistAdapter

--- app/src/main/java/com/todo/behtarinhotel/MainActivity.java
+++ app/src/main/java/com/todo/behtarinhotel/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends BaseMainActivity implements GlobalSearch.Globa
 
     MyPayPall myPayPall;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,27 +37,26 @@ public class MainActivity extends BaseMainActivity implements GlobalSearch.Globa
 //        myPayPall.makeAPayment(this, payPallParams);
 
 
-
-
         GlobalSearch globalSearch = new GlobalSearch();
 
         ListView listView = (ListView) findViewById(R.id.lv_main_list_main_activity);
-        SearchResultSO searchResultSO = new SearchResultSO("Hostel 639",
-                                                            "London",
-                                                            "639 Harrow Road",
-                                                            100,
-                                                            3.5f,
-                                                            10,
+        SearchResultSO searchResultSO = new SearchResultSO("http://images.travelnow.com//hotels/3000000/2840000/2831600/2831521/2831521_59_b.jpg",
+                "Hostel 639",
+                "London",
+                "639 Harrow Road",
+                100,
+                3.5f,
+                10,
                 "http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/2.0-12345-4.gif",
-                                                            "Bla Bla Bla",
-                "http://images.travelnow.com//hotels/3000000/2840000/2831600/2831521/2831521_59_b.jpg");
+                "Bla Bla Bla",
+                42.33f,
+                -71.111336f);
 
         ArrayList<SearchResultSO> searchResultSOArrayList = new ArrayList<>();
         searchResultSOArrayList.add(searchResultSO);
 
         listView.setAdapter(new MainActivityMainListAdapter(this, searchResultSOArrayList));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -87,9 +85,8 @@ public class MainActivity extends BaseMainActivity implements GlobalSearch.Globa
 
     }
 
-
     @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
             if (confirm != null) {
@@ -104,11 +101,9 @@ public class MainActivity extends BaseMainActivity implements GlobalSearch.Globa
                     Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
                 }
             }
-        }
-        else if (resultCode == Activity.RESULT_CANCELED) {
+        } else if (resultCode == Activity.RESULT_CANCELED) {
             Log.i("paymentExample", "The user canceled.");
-        }
-        else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
             Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
         }
     }
--- app/src/main/java/com/todo/behtarinhotel/SearchActivity.java
+++ app/src/main/java/com/todo/behtarinhotel/SearchActivity.java
@@ -1,10 +1,13 @@
 package com.todo.behtarinhotel;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,6 +23,9 @@ public class SearchActivity extends Activity {
     EditText etRoom;
     EditText etAdult;
     EditText etChildren;
+
+    Button btnSearchForHotels;
+
     RangeBar rbStars;
 
     @Override
@@ -39,6 +45,7 @@ public class SearchActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -48,14 +55,23 @@ public class SearchActivity extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+
     }
 
-    private void initViewsById(){
+    private void initViewsById() {
 
         tvCheckIn = (TextView) findViewById(R.id.tv_check_in_search_activity);
         tvCheckOut = (TextView) findViewById(R.id.tv_check_out_search_activity);
+
+        View.OnClickListener oclTextViews = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo get DatePickerDialog
+            }
+        };
+        tvCheckOut.setOnClickListener(oclTextViews);
+        tvCheckIn.setOnClickListener(oclTextViews);
 
         etLocation = (EditText) findViewById(R.id.et_location_search_activity);
         etRoom = (EditText) findViewById(R.id.et_room_search_activity);
@@ -65,6 +81,12 @@ public class SearchActivity extends Activity {
         rbStars = (RangeBar) findViewById(R.id.rb_stars);
         rbStars.setTickCount(5);
 
-
+        btnSearchForHotels = (Button) findViewById(R.id.btn_search_for_hotels_search_activity);
+        btnSearchForHotels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo search for hotels
+            }
+        });
     }
 }
