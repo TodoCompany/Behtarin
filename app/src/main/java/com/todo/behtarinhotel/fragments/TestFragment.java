e09042318ea0b524c7bfb752490d95956b0b503b
Andriy Biskup <AndriyTheProgrammer@gmail.com>
2015-07-23 12:43:52 +0300

    Added BehtarinLoader view

--- app/src/main/java/com/todo/behtarinhotel/fragments/TestFragment.java
+++ app/src/main/java/com/todo/behtarinhotel/fragments/TestFragment.java
@@ -1,14 +1,12 @@
 package com.todo.behtarinhotel.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.todo.behtarinhotel.R;
 
@@ -25,10 +23,8 @@ public class TestFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView text = new TextView(this.getActivity());
-        text.setText("Section");
-        text.setGravity(Gravity.CENTER);
-        return text;
+        View rootView = inflater.inflate(R.layout.fragment_test, null, false);
+        return rootView;
     }
 
 
