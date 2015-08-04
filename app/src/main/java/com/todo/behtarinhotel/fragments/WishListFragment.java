8f24e37a2898a9e8fe9027cd7efa29e1b1ac5df6
LohushD <dmytro.lohush@gmail.com>
2015-08-04 17:36:01 +0300

    start adding wishlist

---
+++ app/src/main/java/com/todo/behtarinhotel/fragments/WishListFragment.java
@@ -0,0 +1,31 @@
+package com.todo.behtarinhotel.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.todo.behtarinhotel.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WishListFragment extends Fragment {
+
+
+    public WishListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wish_list, container, false);
+    }
+
+
+}
